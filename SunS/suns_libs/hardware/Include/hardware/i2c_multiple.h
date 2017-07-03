37eb4d0675c2af83483525fd682c3e0241f2b9d7
Michał Gumiela <michal.gumiela@gmail.com>
2017-07-03 15:09:11 +0200

    I2C multiple driver

--- SunS/suns_libs/hardware/Include/hardware/i2c_multiple.h
+++ SunS/suns_libs/hardware/Include/hardware/i2c_multiple.h
@@ -9,19 +9,19 @@ enum class Action : uint8_t {
     START_READ = 1
 };
 
-template<typename scl, typename sda_a, typename sda_b, typename sda_c, typename sda_d>
+template<typename _scl, typename _sda_a, typename _sda_b, typename _sda_c, typename _sda_d, uint32_t _timeout>
 class SoftI2CMulti : hal::libs::PureStatic {
  public:
     static void init() {
-        scl::init(hal::DigitalIO::Mode::INPUT);
-        sda_a::init(hal::DigitalIO::Mode::INPUT);
-        sda_b::init(hal::DigitalIO::Mode::INPUT);
-        sda_c::init(hal::DigitalIO::Mode::INPUT);
-        sda_d::init(hal::DigitalIO::Mode::INPUT);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
+        _sda_a::init(hal::DigitalIO::Mode::INPUT);
+        _sda_b::init(hal::DigitalIO::Mode::INPUT);
+        _sda_c::init(hal::DigitalIO::Mode::INPUT);
+        _sda_d::init(hal::DigitalIO::Mode::INPUT);
     }
 
     static uint8_t start(uint8_t address, const Action start_action) {
-        scl::init(hal::DigitalIO::Mode::INPUT);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
         _delay_loop_1(hDelay);
         sda_pull_low();
         _delay_loop_1(hDelay);
@@ -32,47 +32,10 @@ class SoftI2CMulti : hal::libs::PureStatic {
     static void stop() {
         sda_pull_low();
         _delay_loop_1(hDelay);
-        scl::init(hal::DigitalIO::Mode::INPUT);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
         _delay_loop_1(qDelay);
         sda_hi_z();
         _delay_loop_1(hDelay);
-    }
-
-    static void shift_bit(bool bit) {
-        scl::reset();
-        scl::init(hal::DigitalIO::Mode::OUTPUT);
-        _delay_loop_1(qDelay);
-
-        if (bit) {
-            sda_hi_z();
-        } else {
-            sda_pull_low();
-        }
-
-        _delay_loop_1(hDelay);
-        scl::init(hal::DigitalIO::Mode::INPUT);
-        _delay_loop_1(hDelay);
-    }
-
-    static bool wait_for_clk_stretching() {
-        volatile uint16_t timeout = 0;
-        while (0 == scl::read()) {
-            if (timeout > 5000) {
-                timeout++;
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static uint8_t read_ack() {
-        uint8_t ack = 0;
-        hal::libs::write_bit<0>(ack, sda_a::read());
-        hal::libs::write_bit<1>(ack, sda_b::read());
-        hal::libs::write_bit<2>(ack, sda_c::read());
-        hal::libs::write_bit<3>(ack, sda_d::read());
-        return ack;
     }
 
     static uint8_t write(uint8_t data) {
@@ -81,53 +44,43 @@ class SoftI2CMulti : hal::libs::PureStatic {
         for (int8_t i = 7; i >= 0; i--) {
             shift_bit(hal::libs::read_bit(data, i));
 
-            if (wait_for_clk_stretching()) {
+            if (wait_for_clk_high()) {
                 hal::libs::write_bit<4>(result, 1);
             }
         }
 
-        scl::reset();
-        scl::init(hal::DigitalIO::Mode::OUTPUT);
+        _scl::reset();
+        _scl::init(hal::DigitalIO::Mode::OUTPUT);
         _delay_loop_1(qDelay);
         sda_hi_z();
         _delay_loop_1(hDelay);
 
-        scl::init(hal::DigitalIO::Mode::INPUT);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
         _delay_loop_1(hDelay);
 
         result |= read_ack();
 
-        scl::reset();
-        scl::init(hal::DigitalIO::Mode::OUTPUT);
+        _scl::reset();
+        _scl::init(hal::DigitalIO::Mode::OUTPUT);
         _delay_loop_1(hDelay);
 
         return result;
     }
 
-    static void scl_low_high() {
-        scl::reset();
-        scl::init(hal::DigitalIO::Mode::OUTPUT);
-        _delay_loop_1(hDelay);
-        scl::init(hal::DigitalIO::Mode::INPUT);
-        _delay_loop_1(hDelay);
-    }
-
-    static std::array<uint8_t, 4> read(bool ack) {
+    static bool read(std::array<uint8_t, 4>& data, bool ack) {
         std::array<uint8_t, 4> SDA_read_data = {0};
-
         for (int8_t i = 7; i >= 0; i--) {
-            scl_low_high();
-            while (0 == scl::read()) {
-            }
+            _scl_low_high();
+            wait_for_clk_high();
 
-            wb(SDA_read_data[0], sda_a::read(), i);
-            wb(SDA_read_data[1], sda_b::read(), i);
-            wb(SDA_read_data[2], sda_c::read(), i);
-            wb(SDA_read_data[3], sda_d::read(), i);
+            write_bit_runtime(SDA_read_data[0], _sda_a::read(), i);
+            write_bit_runtime(SDA_read_data[1], _sda_b::read(), i);
+            write_bit_runtime(SDA_read_data[2], _sda_c::read(), i);
+            write_bit_runtime(SDA_read_data[3], _sda_d::read(), i);
         }
 
-        scl::reset();
-        scl::init(hal::DigitalIO::Mode::OUTPUT);
+        _scl::reset();
+        _scl::init(hal::DigitalIO::Mode::OUTPUT);
         _delay_loop_1(qDelay);
 
         if (true == ack) {
@@ -138,47 +91,93 @@ class SoftI2CMulti : hal::libs::PureStatic {
 
         _delay_loop_1(hDelay);
 
-        scl::init(hal::DigitalIO::Mode::INPUT);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
         _delay_loop_1(hDelay);
 
-        scl::reset();
-        scl::init(hal::DigitalIO::Mode::OUTPUT);
+        _scl::reset();
+        _scl::init(hal::DigitalIO::Mode::OUTPUT);
 
         sda_hi_z();
         _delay_loop_1(hDelay);
+        data = SDA_read_data;
 
-        return SDA_read_data;
+        return false;
     }
 
  private:
-    static constexpr uint8_t qDelay{3};
-    static constexpr uint8_t hDelay{5};
+    static constexpr uint8_t qDelay{30};
+    static constexpr uint8_t hDelay{50};
 
     static void sda_pull_low() {
-        sda_a::reset();
-        sda_b::reset();
-        sda_c::reset();
-        sda_d::reset();
+        _sda_a::reset();
+        _sda_b::reset();
+        _sda_c::reset();
+        _sda_d::reset();
 
-        sda_a::init(hal::DigitalIO::Mode::OUTPUT);
-        sda_b::init(hal::DigitalIO::Mode::OUTPUT);
-        sda_c::init(hal::DigitalIO::Mode::OUTPUT);
-        sda_d::init(hal::DigitalIO::Mode::OUTPUT);
+        _sda_a::init(hal::DigitalIO::Mode::OUTPUT);
+        _sda_b::init(hal::DigitalIO::Mode::OUTPUT);
+        _sda_c::init(hal::DigitalIO::Mode::OUTPUT);
+        _sda_d::init(hal::DigitalIO::Mode::OUTPUT);
     }
 
     static void sda_hi_z() {
-        sda_a::init(hal::DigitalIO::Mode::INPUT);
-        sda_b::init(hal::DigitalIO::Mode::INPUT);
-        sda_c::init(hal::DigitalIO::Mode::INPUT);
-        sda_d::init(hal::DigitalIO::Mode::INPUT);
+        _sda_a::init(hal::DigitalIO::Mode::INPUT);
+        _sda_b::init(hal::DigitalIO::Mode::INPUT);
+        _sda_c::init(hal::DigitalIO::Mode::INPUT);
+        _sda_d::init(hal::DigitalIO::Mode::INPUT);
     }
 
-    static void wb(uint8_t& dest, uint8_t val, uint8_t pos) {
+    static inline void write_bit_runtime(uint8_t& dest, bool val, uint8_t pos) {
         if (val) {
             dest |= (val << pos);
         } else {
             dest &= ~(val << pos);
         }
+    }
+
+    static void shift_bit(bool data_bit) {
+        _scl::reset();
+        _scl::init(hal::DigitalIO::Mode::OUTPUT);
+        _delay_loop_1(qDelay);
+
+        if (data_bit) {
+            sda_hi_z();
+        } else {
+            sda_pull_low();
+        }
+
+        _delay_loop_1(hDelay);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
+        _delay_loop_1(hDelay);
+    }
+
+    static bool wait_for_clk_high() {
+        volatile uint32_t timeout = 0;
+        while (0 == _scl::read()) {
+            if (timeout < _timeout) {
+                timeout++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static uint8_t read_ack() {
+        uint8_t ack = 0;
+        hal::libs::write_bit<0>(ack, _sda_a::read());
+        hal::libs::write_bit<1>(ack, _sda_b::read());
+        hal::libs::write_bit<2>(ack, _sda_c::read());
+        hal::libs::write_bit<3>(ack, _sda_d::read());
+        return ack;
+    }
+
+    static void _scl_low_high() {
+        _scl::reset();
+        _scl::init(hal::DigitalIO::Mode::OUTPUT);
+        _delay_loop_1(hDelay);
+        _scl::init(hal::DigitalIO::Mode::INPUT);
+        _delay_loop_1(hDelay);
     }
 
 };
