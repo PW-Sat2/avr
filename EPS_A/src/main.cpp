0543f1a86f83f053cfd4263a5341ba711786af4a
Grzegorz Gajoch <gajoch@gmail.com>
2017-08-08 00:48:39 +0100

    Added ATOMIC_BLOCKs for thread-safety in main

--- EPS_A/src/main.cpp
+++ EPS_A/src/main.cpp
@@ -4,12 +4,34 @@
 #include "eps/eps.h"
 #include "eps/main.h"
 
+void each_33ms();
+void each_1sec();
+
+
 eps_a::Telemetry telemetry;
 EPSACommandDispatcher dispatcher;
 
 ISR(TWI_vect) {
     Obc::process_interrupt();
     dispatcher.dispatch();
+}
+
+avr::Prescaler<33> timer_1second;
+void each_33ms() {
+    TelemetryUpdater::update_mppt();
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+    }
+
+    if (timer_1second.expired()) {
+        each_1sec();
+    }
+}
+
+void each_1sec() {
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+        ThermalKnives::tick();
+    }
+    TelemetryUpdater::update_general();
 }
 
 int main() {
@@ -20,17 +42,11 @@ int main() {
 
     sei();
 
-    avr::Prescaler<33> timer_1second;
-
     LOG_INFO("EPS A initialized.");
 
     while (1) {
         if (eps_a::MainTimer::expired()) {
-            TelemetryUpdater::update_mppt();
-            if (timer_1second.expired()) {
-                ThermalKnives::tick();
-                TelemetryUpdater::update_general();
-            }
+            each_33ms();
         }
     }
 
