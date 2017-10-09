bd27123490f879d6f39ec2554c71fde45b63d795
Grzegorz Gajoch <gajoch@gmail.com>
2017-10-09 01:09:06 +0100

    Logs added

--- libs/eps_commands/commands/DisablePowerCycle.h
+++ libs/eps_commands/commands/DisablePowerCycle.h
@@ -13,10 +13,13 @@ constexpr static uint8_t disable_code[] = {0xBA, 0xAD, 0xC0, 0x01, 0xC0, 0xDE};
 template<bool& flag_armed>
 struct ArmDisablePowerCycle : public Command<0xEA, 5> {
     void invoke(gsl::span<const std::uint8_t> cmd) {
+        LOG_FATAL("ARM power cycle disabling command received");
         if (cmd == gsl::make_span(arm_code)) {
             flag_armed = true;
+            LOG_FATAL("Power Cycle Disable ARMED");
         } else {
             flag_armed = false;
+            LOG_FATAL("Power Cycle Disable DISARMED");
         }
     }
 };
@@ -24,11 +27,14 @@ struct ArmDisablePowerCycle : public Command<0xEA, 5> {
 template<const bool& flag_armed, bool& flag_disabled>
 struct DisablePowerCycle : public Command<0xEB, 5> {
     void invoke(gsl::span<const std::uint8_t> cmd) {
+        LOG_FATAL("Disable power cycle command received");
         if (hal::libs::ToInt(flag_armed) == 1 &&
             cmd == gsl::make_span(disable_code)) {
             flag_disabled = true;
+            LOG_FATAL("Power Cycle Disabled!");
         } else {
             flag_disabled = false;
+            LOG_FATAL("Power Cycle Enabled!");
         }
     }
 };
