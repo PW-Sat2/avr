a03f0a4e771e050565e57d239c0781499bb0a569
Grzegorz Gajoch <gajoch@gmail.com>
2017-10-10 04:00:23 +0100

    Added Serial RX pin to check loop duty

--- libs/eps/periodics.cpp
+++ libs/eps/periodics.cpp
@@ -56,9 +56,12 @@ void each_30min() {
 }
 
 void Eps::run() {
+    Eps::IOMap::SerialRx::init(hal::DigitalIO::Mode::OUTPUT);
     while (1) {
         if (eps::MainTimer::expired()) {
+            Eps::IOMap::SerialRx::set();
             each_33ms();
+            Eps::IOMap::SerialRx::reset();
         }
     }
 }
